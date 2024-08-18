4be6f049f04bf315d5749f7776e0959ca667e95a
Kelvin Severino <kseveri@outlook.com>
2024-08-18 14:54:56 -0300

    feat: include cors for react connection

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
 import auth from "./authRoute.js";
 import users from "./usersRoute.js";
 import heros from "./herosRoute.js";
 
 const routes = (app) => {
+    app.use(cors({
+        origin: 'http://localhost:3000', // URL do seu aplicativo React
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }));
+
     app.route("/").get((req, res) => res.status(200)
                                         .send("Seja bem-vindo a API iHeros"));
 
